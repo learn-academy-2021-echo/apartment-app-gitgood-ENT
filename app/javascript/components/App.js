bd27a48971a622813f5e8fd159e99dae717dcce5
gitgood-ENT <ward.noahd@gmail.com>
2022-02-10 11:27:47 -0800

    actually installed resources

---
+++ app/javascript/components/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+import Header from "./components/Header"
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <>  
+        <Header />
+        <h1 className="welcomeTo">Find the perfect apartment</h1>
+      
+
+      </>
+    );
+  }
+}
+
+export default App
---
+++ app/javascript/components/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import SignInOut from "./SignInOut";
+
+export default class Header extends Component {
+  render() {
+    return (
+        <>
+            <div>Header</div>
+            <SignInOut />
+        </>
+    )
+  }
+}
